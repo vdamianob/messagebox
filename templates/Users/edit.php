fbb1b84dac9083e0e8e9c130d6b56feeebaaca39
vdamianob <vittorio.brasini@studio.unibo.it>
2025-05-23 12:19:55 +0200

    style: restructure user edit form layout; remove password and last login fields

--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -13,21 +13,24 @@
 <?php $this->assign('tb_sidebar', '<ul class="nav flex-column">' . $this->fetch('tb_actions') . '</ul>'); ?>
 
 <div class="users form content">
-    <?= $this->Form->create($user) ?>
-    <fieldset>
-        <legend><?= __('Edit User') ?></legend>
-        <?php
-            echo $this->Form->control('email');
-            echo $this->Form->control('password');
-            echo $this->Form->control('role');
-            echo $this->Form->control('status');
-            echo $this->Form->control('username');
-            echo $this->Form->control('avatar_img_path');
-            echo $this->Form->control('note');
-            echo $this->Form->control('last_login');
-            echo $this->Form->control('deleted', ['empty' => true]);
-        ?>
-    </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
-    <?= $this->Form->end() ?>
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-6">
+            <?= $this->Form->create($user) ?>
+            <fieldset>
+                <legend><?= __('Edit User') ?></legend>
+                <?php
+                    echo $this->Form->control('email');
+                    echo $this->Form->control('role');
+                    echo $this->Form->control('status');
+                    echo $this->Form->control('username');
+                    echo $this->Form->control('avatar_img_path');
+                    echo $this->Form->control('note');
+                    //echo $this->Form->control('deleted', ['empty' => true]);
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary mt-3']) ?>
+            <?= $this->Html->link(__('Change Password'), ['action' => 'changePassword', $user->id], ['class' => 'btn btn-secondary mt-3 ms-2']) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
 </div>
